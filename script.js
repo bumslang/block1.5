8fc578cd399e2b923ff33b418f530cdf83fd6d17
bumslang <65829331+bumslang@users.noreply.github.com>
2020-07-03 16:20:14 +0300

    Обновил index.html и style.css

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
+const slider = document.querySelector('.swiper-container');
+
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
 let mobile = window.matchMedia("(min-width: 768px)");
 mobile.addListener(addElem);
-let desktop = window.matchMedia("(min-width: 1015px)");
+let desktop = window.matchMedia("(min-width: 1016px)");
 desktop.addListener(hideElem);
-let bigResolution = window.matchMedia("(min-width: 1265px)");
+let bigResolution = window.matchMedia("(min-width: 1500px)");
 bigResolution.addListener(hideElem);
 
-if (mobile.matches){
-    addElem();
+
+  if (mobile.matches) {
+    addElem()
+  }
+  function addElem(){
+  if (mobile.matches){
+    let swiperSlide = document.querySelectorAll('.swiper-slide');
+    let sliderContainer = document.querySelector('.slider-container');
+    let swiperWripper = document.querySelector('.swiper-wrapper');
+    let swiperContainer = document.querySelector('.swiper-container')
+    for (let i = 0; i < swiperSlide.length; i++) {
+        swiperSlide[i].classList.remove('swiper-slide');
+        swiperSlide[i].removeAttribute("style")
+    }
+    sliderContainer.classList.remove('slider-container')
+    sliderContainer.classList.add('brends');
+
+    swiperWripper.classList.remove('swiper-wrapper')
+    swiperWripper.classList.add('brends__wripper');
+
+    swiperContainer.classList.remove('swiper-container')
+    swiperContainer.classList.add('brends__container')
+    hideElem()
+  } else{
+    let swiperSlide = document.querySelectorAll('.brends__elem');
+    let brends = document.querySelector('.brends');
+    let brendsWripper = document.querySelector('.brends__wripper');
+    let brendsContainer = document.querySelector('.brends__container')
+    for (let i = 0; i < swiperSlide.length; i++) {
+        swiperSlide[i].classList.add('swiper-slide')
+    } 
+    brends.classList.remove('brends')
+    brends.classList.add('slider-container')
+
+    brendsWripper.classList.remove('brends__wripper')
+    brendsWripper.classList.add('swiper-wrapper');
+
+    brendsContainer.classList.remove('brends__container');
+    brendsContainer.classList.add('swiper-container')
+
+}
 }
 
-function addElem(){
-    let swiperSlide = document.querySelectorAll('.swiper-slide');
-    let swiperWriper = document.querySelector(".swiper-wrapper");
-    if (mobile.matches){
-        swiperWriper.className = 'brends brends__sizes';
-        for (let i = 0; i < swiperSlide.length; i++) {
-            swiperSlide[i].removeAttribute('style')
-            swiperSlide[i].className = "brends__item";
-        }
-        hideElem();
-    } else {
-        location.reload();
-    }
-}
+
+//-------------------------показать все-------------------------------------
+
 
 let showMore = document.querySelector('.show-more');
 showMore.addEventListener("click", showBrends)
 
 function showBrends(item){
-    let brendsItem = document.querySelectorAll(".brends__item");
+    let brendsItem = document.querySelectorAll(".brends__elem");
     let showMoreText = document.querySelector(".show-more__text");
     let showMoreIcon = document.querySelector(".show-more__icon")
-    if (document.body.clientWidth >= 768 && document.body.clientWidth <= 1015){
+    if (mobile.matches && !(desktop.matches)){
         item = 6;
-    } else if (document.body.clientWidth >= 1016 && document.body.clientWidth <= 1264){
+    } else if (desktop.matches && mobile.matches && !bigResolution.matches){
         item = 8;
-    } else {
+    } else if (bigResolution.matches){
         item = 10;
     }
-    
     for (let i = item; i < brendsItem.length; i++) {
         brendsItem[i].classList.toggle("brends__item--hide");
     }
@@ -52,23 +88,25 @@ function showBrends(item){
     }
 }
 
-function hideElem(numb) {
-    let brendsItem = document.querySelectorAll(".brends__item");
-    if(mobile.matches && !(desktop.matches)){
-        numb = 6;
-    } else if(desktop.matches && mobile.matches && !bigResolution.matches){
-        numb = 8;
-        brendsItem[6].classList.remove("brends__item--hide");
-        brendsItem[7].classList.remove("brends__item--hide");
-    } else if (bigResolution.matches){
-        numb = 10;
-        brendsItem[6].classList.remove("brends__item--hide");
-        brendsItem[7].classList.remove("brends__item--hide");
-        brendsItem[8].classList.remove("brends__item--hide");
-        brendsItem[9].classList.remove("brends__item--hide");
-    }
-    for (let i = numb; i < brendsItem.length; i++) {
-        brendsItem[i].classList.add("brends__item--hide");
-    }   
-}
 
+// ----------------------- Прячем элементы ---------------------------
+
+function hideElem(numb) {
+  let brendsItem = document.querySelectorAll(".brends__elem");
+  if(mobile.matches && !(desktop.matches)){
+      numb = 6;
+  } else if(desktop.matches && mobile.matches && !bigResolution.matches){
+      numb = 8;
+      brendsItem[6].classList.remove("brends__item--hide");
+      brendsItem[7].classList.remove("brends__item--hide");
+  } else if (bigResolution.matches){
+      numb = 10;
+      brendsItem[6].classList.remove("brends__item--hide");
+      brendsItem[7].classList.remove("brends__item--hide");
+      brendsItem[8].classList.remove("brends__item--hide");
+      brendsItem[9].classList.remove("brends__item--hide");
+  }
+  for (let i = numb; i < brendsItem.length; i++) {
+      brendsItem[i].classList.add("brends__item--hide");
+  }   
+}

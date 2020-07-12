12e139b6f1c8a6eeccda2c9db024ba2f5a300c80
bumslang <65829331+bumslang@users.noreply.github.com>
2020-07-12 23:38:57 +0300

    Изменил после ревью

--- script.js
+++ script.js
@@ -1,83 +1,36 @@
-function getBrends() {
-const slider = document.querySelector('.swiper-container');
-var swiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  });
+const brendsSlider = document.querySelector('.brends__container');
+const mobile = window.matchMedia("(max-width: 767px)");
 
-let mobile = window.matchMedia("(min-width: 768px)");
-mobile.addListener(addElem);
-let desktop = window.matchMedia("(min-width: 1016px)");
-desktop.addListener(hideElem);
-let bigResolution = window.matchMedia("(min-width: 1500px)");
-bigResolution.addListener(hideElem);
+let swiper;
 
+mobile.addEventListener('change', destroySlider);
 
-  if (mobile.matches) {
-    addElem();
-  }
-  function addElem(){
+destroySlider();
+
+function destroySlider(){
   if (mobile.matches){
-    let swiperSlide = document.querySelectorAll('.swiper-slide');
-    let sliderContainer = document.querySelector('.slider-container');
-    let swiperWripper = document.querySelector('.swiper-wrapper');
-    let swiperContainer = document.querySelector('.swiper-container');
-    for (let i = 0; i < swiperSlide.length; i++) {
-        swiperSlide[i].classList.remove('swiper-slide');
-        swiperSlide[i].removeAttribute("style");
-    }
-    sliderContainer.classList.remove('slider-container');
-    sliderContainer.classList.add('brends');
-
-    swiperWripper.classList.remove('swiper-wrapper');
-    swiperWripper.classList.add('brends__wripper');
-
-    swiperContainer.classList.remove('swiper-container');
-    swiperContainer.classList.add('brends__container');
-    hideElem()
-  } else{
-    let swiperSlide = document.querySelectorAll('.brends__elem');
-    let brends = document.querySelector('.brends');
-    let brendsWripper = document.querySelector('.brends__wripper');
-    let brendsContainer = document.querySelector('.brends__container');
-    for (let i = 0; i < swiperSlide.length; i++) {
-        swiperSlide[i].classList.add('swiper-slide');
-    } 
-    brends.classList.remove('brends');
-    brends.classList.add('slider-container');
-
-    brendsWripper.classList.remove('brends__wripper');
-    brendsWripper.classList.add('swiper-wrapper');
-
-    brendsContainer.classList.remove('brends__container');
-    brendsContainer.classList.add('swiper-container');
-
-}
+    swiper = new Swiper(brendsSlider, {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  } else if(brendsSlider.classList.contains('swiper-container-initialized')){
+    swiper.destroy()
+  }
 }
 
+const showMore = document.querySelector('.show-more')
+const brendsWrapper = document.querySelector(".brends__wrapper");
+const showMoreText = document.querySelector(".show-more__text");
+const showMoreIcon = document.querySelector(".show-more__icon");
 
-//-------------------------показать все-------------------------------------
+showMore.addEventListener('click', showElem);
 
-
-let showMore = document.querySelector('.show-more');
-showMore.addEventListener("click", showBrends);
-
-function showBrends(item){
-    let brendsItem = document.querySelectorAll(".brends__elem");
-    let showMoreText = document.querySelector(".show-more__text");
-    let showMoreIcon = document.querySelector(".show-more__icon")
-    if (mobile.matches && !(desktop.matches)){
-        item = 6;
-    } else if (desktop.matches && mobile.matches && !bigResolution.matches){
-        item = 8;
-    } else if (bigResolution.matches){
-        item = 10;
-    }
-    for (let i = item; i < brendsItem.length; i++) {
-        brendsItem[i].classList.toggle("brends__item--hide");
-    }
-
+function showElem(){
+  brendsWrapper.classList.toggle('brends__wrapper--height');
     if (showMoreText.textContent == "Показать все") {
         showMoreText.textContent = "Скрыть";
         showMoreIcon.style.transform = "rotate(180deg)";
@@ -86,28 +39,3 @@ function showBrends(item){
         showMoreIcon.style.transform = "";
     }
 }
-
-
-// ----------------------- Прячем элементы ---------------------------
-
-function hideElem(numb) {
-  let brendsItem = document.querySelectorAll(".brends__elem");
-  if(mobile.matches && !(desktop.matches)){
-      numb = 6;
-  } else if(desktop.matches && mobile.matches && !bigResolution.matches){
-      numb = 8;
-      brendsItem[6].classList.remove("brends__item--hide");
-      brendsItem[7].classList.remove("brends__item--hide");
-  } else if (bigResolution.matches){
-      numb = 10;
-      brendsItem[6].classList.remove("brends__item--hide");
-      brendsItem[7].classList.remove("brends__item--hide");
-      brendsItem[8].classList.remove("brends__item--hide");
-      brendsItem[9].classList.remove("brends__item--hide");
-  }
-  for (let i = numb; i < brendsItem.length; i++) {
-      brendsItem[i].classList.add("brends__item--hide");
-  }   
-}
-}
-getBrends();
